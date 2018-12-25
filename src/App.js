c58d467cb9a5b6c5a06459d89f999d014e9739e9
reza <rezakam64@yahoo.com>
2018-12-25 12:15:09 +0330

    f

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Header from "./Header/Header";
 
 class App extends Component {
   state={
-    Persons:[{ name:"reza", age:18},{ name:"ali", age:18},{ name:"ahmad", age:18}]
+    Persons:[
+        { id:1,name:"reza", age:18},
+        { id:2,name:"ali", age:18},
+        { id:3,name:"ahmad", age:18}],
+      showPerson: false
   }
+
   checkClick=(name)=>{
    this.setState({
      Persons:[{ name:name, age:10},{ name:"ali", age:18},{ name:"ahmad", age:18}]
@@ -19,15 +24,75 @@ class App extends Component {
       Persons:[{ name:event.target.value, age:10},{ name:"ali", age:18},{ name:"ahmad", age:18}]
     })
   }
+    togglePerson = () =>{
+      const status=this.state.showPerson;
+        this.setState({
+            showPerson : !status
+        })
+    }
+
+    deletePerson = (index) =>{
+        const Allperson=this.state.Persons;
+        //const Allperson=[...this.state.Persons];
+        Allperson.splice(index,1);
+        this.setState({
+            Persons : Allperson
+        })
+    }
+    nameChangeHandler=(event,index)=>{
+      const personIndex=this.state.Persons.findIndex(p=>{
+          return p.id===index
+      })
+        const PersonValue=this.state.Persons[personIndex];
+        PersonValue.name=event.target.value;
+      const Person=[...this.state.Persons];
+      Person[personIndex]=PersonValue;
+      this.setState({Person:Person});
+    }
 
 
-  render() {
+
+    render() {
+        const style = {
+            backgroundColor: 'green',
+            color:'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer',
+        };
+      let persons=null;
+      const styles=[];
+      if(this.state.Persons.length<=2)
+          styles.push('red')
+        if(this.state.Persons.length<=1)
+            styles.push('bold')
+      if(this.state.showPerson){
+          persons=(
+              <div>
+                  {this.state.Persons.map((person,index)=>{
+                      return <Header
+                          name={person.name}
+                          age={person.age}
+                          key={person.id}
+                          oncl={()=>this.deletePerson(index)}
+                      onchange={(event)=>this.nameChangeHandler(index)}/>
+                  })}
+                 {/* <Header name={this.state.Persons[0].name} age={this.state.Persons[0].age} oncl={this.onclickChange} />
+                  <Header name={this.state.Persons[1].name} age={this.state.Persons[1].age} oncl={this.onclickChange} />
+                  <Header name={this.state.Persons[2].name} age={this.state.Persons[2].age} oncl={this.onclickChange} />*/}
+              </div>
+          );
+          style.backgroundColor='red';
+      }
     return (
       <div className="App">
-        <Header name={this.state.Persons[0].name} age={this.state.Persons[0].age} oncl={this.onclickChange} />
-        <h1>TEST</h1>
-        <button onClick={this.checkClick.bind(this,'alireza')}>click</button>
-        <button onClick={()=>this.checkClick('alireza')}>click</button>
+          <p className={styles.join(' ')}>please click button</p>
+          {persons}
+
+       {/* <button onClick={this.checkClick.bind(this,'alireza')}>click</button>
+        <button onClick={()=>this.checkClick('alireza')}>click</button>*/}
+          <button onClick={this.togglePerson} style={style}>نمایش پرسنل</button>
 
         {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
